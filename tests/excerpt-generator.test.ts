de5bbe4b73e8247c39c5412297df62113e42647e
Michel Weimerskirch <michel@weimerskirch.net>
2022-09-14 22:24:43 +0200

    Added a test case that handles an overlap

--- tests/excerpt-generator.test.ts
+++ tests/excerpt-generator.test.ts
@@ -4,9 +4,10 @@ import {createExcerpt} from "../src/excerpt-generator";
 let singleMatchSearch = "lobortis";
 let noMatchSearch = "loremipsum";
 let multipleMatchSearch = "est";
+let multipleMatchWithOverlapSearch = "nibh";
 
 let shortInput = "Lorem ipsum dolor sit amet, consectetur adipiscing elit.";
-let longInput = "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Cras lobortis eros eget est vulputate, nec blandit nunc lacinia. Vestibulum finibus maximus nisi ultricies efficitur. Duis purus nibh, porta a dictum ut, dapibus non erat. Nunc vehicula metus nec gravida lacinia. Vestibulum faucibus pharetra semper. Morbi lacinia et mauris ac vulputate. Nulla justo ligula, finibus ut dolor eu, aliquam lacinia ante. Pellentesque ultrices eros non gravida pulvinar. Ut hendrerit, est nec consequat suscipit, erat nulla pharetra neque, in luctus tellus justo a dui. Ut feugiat, turpis efficitur pellentesque rutrum, nibh magna tristique arcu, sed tincidunt dui turpis ut urna. Duis commodo nec velit eget fringilla. Proin imperdiet varius consectetur. Vivamus sit amet pharetra velit, vitae aliquet libero. Suspendisse consequat ligula tortor, quis tempus lacus tempus in.";
+let longInput = "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Cras lobortis eros eget est vulputate, nec blandit nunc lacinia. Vestibulum finibus maximus nisi ultricies efficitur. Duis purus nibh, porta a dictum ut, dapibus non erat. Nunc vehicula metus nec gravida lacinia. Vestibulum faucibus pharetra semper. Morbi lacinia et mauris ac vulputate. Nulla justo ligula, finibus ut dolor eu, aliquam lacinia ante. Pellentesque ultrices eros non gravida pulvinar. Ut hendrerit, est nec consequat suscipit, erat nulla pharetra neque, in luctus tellus justo a dui. Ut feugiat, turpis efficitur pellentesque rutrum, nibh magna tristique arcu, sed tincidunt dui turpis ut nibh. Duis commodo nec velit eget fringilla. Proin imperdiet varius consectetur. Vivamus sit amet pharetra velit, vitae aliquet libero. Suspendisse consequat ligula tortor, quis tempus lacus tempus in.";
 
 describe('testing excerpt generation', () => {
     test('no match on short string should return entire string', () => {
@@ -20,5 +21,8 @@ describe('testing excerpt generation', () => {
     });
     test('multiple matches should return a longer excerpt with an ellipsis in between', () => {
         expect(createExcerpt(longInput, multipleMatchSearch, 5, 200)).toBe('… elit. Cras lobortis eros eget <strong>est</strong> vulputate, nec blandit nunc lacinia… non gravida pulvinar. Ut hendrerit, <strong>est</strong> nec consequat suscipit, erat nulla …');
+    });
+    test('multiple matches with overlap should return a longer excerpt without an ellipsis', () => {
+        expect(createExcerpt(longInput, multipleMatchWithOverlapSearch, 5, 200)).toBe('… nisi ultricies efficitur. Duis purus <strong>nibh</strong>, porta a dictum ut, dapibus… feugiat, turpis efficitur pellentesque rutrum, <strong>nibh</strong> magna tristique arcu, sed tinciduntdui turpis ut <strong>nibh</strong>. Duis commodo nec velit eget …');
     });
 });
